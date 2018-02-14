a75d9933bb9775875eb8e405eecc9844f3f1f7ec
Scott Silverman <scottms724@gmail.com>
2018-02-14 20:00:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $(input).on('keydown', function(key) {
+  $('input').on('keydown', function(key) {
     if (key.which == 71) {
       alert("You have pressed the G key!");
     }
